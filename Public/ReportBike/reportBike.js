d7935ffd960fa17b431a5e5bf2f64bb20f0d53ea
Olivia <36156021+OA1996@users.noreply.github.com>
2018-05-10 15:10:27 +0100

    part of google maps javascript not complete

--- Public/ReportBike/reportBike.js
+++ Public/ReportBike/reportBike.js
@@ -113,3 +113,40 @@ function changeBikeStatus(serialNo) {
         };
     };
 };
+
+var mapCenter = new google.maps.LatLng(51.8979988098144,-2.0838599205017);
+var geocoder = new google.maps.Geocoder();
+var infowindow = new google.maps.InfoWindow();
+var marker;
+
+function initialize() {
+    var mapOptions = {
+        zoom: 15,
+        center: mapCenter
+    };
+
+    myMap = new google.maps.Map(document.getElementById("mapInput"), mapOptions);
+
+    marker = new google.maps.Marker({
+        map: myMap,
+        position: mapCenter,
+        draggable: true
+    });
+
+    google.maps.event.addListener(marker,'dragend', markerDragged);
+
+    function markerDragged() {
+        var selectedPos = {'latLng': marker.getPosition()};
+        
+    }
+
+    function showAddressInInfoWindow(results) {
+        if (results[0]) {
+            infowindow.setContent(results[0].formatted_address);
+            infowindow.open(myMap, marker);
+        
+        }
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
